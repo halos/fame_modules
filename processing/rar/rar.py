4c7fff150e8c5a0df7153b6adc52400f9178525a
Javier Rascon <halos@sonar>
2019-09-12 13:37:52 +0200

    Initial commit

---
+++ processing/rar/rar.py
@@ -0,0 +1,37 @@
+import os
+from rarfile import RarFile
+
+from fame.core.module import ProcessingModule
+from fame.common.utils import tempdir
+
+
+class Zip(ProcessingModule):
+    name = "rar"
+    description = "Extract files from RAR archive."
+    acts_on = "rar"
+
+    def each(self, target):
+        tmpdir = tempdir()
+
+        rf = RarFile(target)
+
+        namelist = rf.namelist()
+
+        for name in namelist:
+            try:
+                filepath = rf.extract(name, tmpdir)
+                if os.path.isfile(filepath):
+                    self.add_extracted_file(filepath)
+            except RuntimeError:
+                for password in ['virus', 'infected']:
+                    try:
+                        filepath = rf.extract(name, tmpdir, pwd=password)
+                        if os.path.isfile(filepath):
+                            self.add_extracted_file(filepath)
+                        break
+                    except RuntimeError:
+                        pass
+                else:
+                    self.log('error', 'Could not extract {}'.format(name))
+
+        return True
