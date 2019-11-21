c7b37c9f6a98f6b32095fa0297ec3cfb068255dd
Javier Rascon <halos@sonar>
2019-11-21 17:24:42 +0100

    Added code to export hexdump results to html report

--- processing/yara_raw/yara_raw.py
+++ processing/yara_raw/yara_raw.py
@@ -44,30 +44,35 @@ class YaraRaw(ProcessingModule):
 
     def show_hexdump(self, target, output):
 
+        matches = {}
         patt_full_rule = r"(?P<rule>\w+?) (?:.+?)\n(?P<matches>(?:(?:0x[0-9a-fA-F]+?):\$(?:.*?): (?:.*?)(?:\n|$))+)"
         patt_condition = r"(?:(0x.*?):\$(.*?): .*?(?:\n|$))+?"
 
-        for rule_name, conditions in re.findall(patt_full_rule, output):
-            self.log("WARNING", "rule: {}".format(rule_name))
-            for offset, condition in re.findall(patt_condition, conditions):
-                self.log("WARNING", "condition: {} @ {}".format(condition, offset))
+        for rule_name, conditions in re.findall(patt_full_rule, output):    
+            matches[rule_name] = []
+            
+            for offset_str, condition in re.findall(patt_condition, conditions):
                 with open(target, "rb") as fd:
-                    offset = int(offset, 16)
+                    offset = int(offset_str, 16)
                     offset = max(offset-32, 0)
 
                     fd.seek(offset, 0)
                     buff = fd.read(16*5)
-                    self.log("WARNING", "hexdump: {}".format(hexdump.hexdump(buff, result="return")))
+                    hex_str = hexdump.hexdump(buff, result="return")
+                    matches[rule_name].append((condition, offset_str, hex_str))
+
+        return matches
 
     def each(self, target):
         
+        self.results = {}
         yara_version = self.get_yara_version()
 
         # version > 3.9
         if yara_version >= 3009:
-            args = [self.bin_path, "-C", self.compiled_rules, target]
+            args = [self.bin_path, "-s", "-C", self.compiled_rules, target]
         else:
-            args = [self.bin_path, self.compiled_rules, target]
+            args = [self.bin_path, "-s", self.compiled_rules, target]
 
         scan_proc = subprocess.Popen(
             args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -79,12 +84,10 @@ class YaraRaw(ProcessingModule):
         if len(stdout) == 0:
             return False
 
-        self.show_hexdump(target, stdout)
+        matches = self.show_hexdump(target, stdout)
+        self.results["matches"] = matches
 
-        lines = stdout.strip().split('\n')
-
-        for line in lines:
-            rule = line.split()[0]
+        for rule in matches.keys():
             self.add_tag(rule)
 
         return True
